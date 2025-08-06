a9fb34d7dda59de8c90c6f5aa12cb6e2cd7fab93
Sunwoo Gwon <tnsdn302@naver.com>
2025-08-06 11:20:50 -0400

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,13 +1,23 @@
-apt update
-apt upgrade -y
-apt install -y vim
-apt install -y build-essential
-apt install -y git
-apt install -y cmake
-apt install -y libx11-dev libxpm-dev libxft-dev libxext-dev
-apt install -y libssl-dev
-apt install -y python3-dev
-apt install -y cmake-curses-gui
-apt install -y libxerces-c-dev
-apt install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
-apt install -y libxmu-dev libxmu-headers
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl
+sudo apt install -y dconf-cli
+sudo apt install -y npm
+sudo apt install -y vim
+sudo apt install -y build-essential
+sudo apt install -y git
+sudo apt install -y cmake
+sudo apt install -y libx11-dev libxpm-dev libxft-dev libxext-dev
+sudo apt install -y libssl-dev
+sudo apt install -y python3-dev
+sudo apt install -y cmake-curses-gui
+sudo apt install -y libxerces-c-dev
+sudo apt install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+sudo apt install -y libxmu-dev libxmu-headers
+
+git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./install.sh
+
+cd ..
+./setup.sh
