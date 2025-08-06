a76e878ad72edeaeaf4e50426366cecc889825d3
Sunwoo Gwon <tnsdn302@naver.com>
2025-08-06 10:59:17 -0400

    add ubuntu setup

---
+++ setup_ubuntu.sh
@@ -0,0 +1,13 @@
+apt update
+apt upgrade -y
+apt install -y vim
+apt install -y build-essential
+apt install -y git
+apt install -y cmake
+apt install -y libx11-dev libxpm-dev libxft-dev libxext-dev
+apt install -y libssl-dev
+apt install -y python3-dev
+apt install -y cmake-curses-gui
+apt install -y libxerces-c-dev
+apt install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+apt install -y libxmu-dev libxmu-headers
